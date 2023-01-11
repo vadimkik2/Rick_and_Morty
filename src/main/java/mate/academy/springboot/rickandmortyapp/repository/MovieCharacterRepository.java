2296e6853045354e7ab1b2ce1f7fbd61a899bfea
vadimkik2 <vadimkik@ukr.net>
2023-01-11 22:41:06 +0200

    Fixed my code.

--- src/main/java/mate/academy/springboot/rickandmortyapp/repository/MovieCharacterRepository.java
+++ src/main/java/mate/academy/springboot/rickandmortyapp/repository/MovieCharacterRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Set;
 import mate.academy.springboot.rickandmortyapp.model.MovieCharacter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovieCharacterRepository extends JpaRepository<MovieCharacter,Long> {
     List<MovieCharacter> findAllByExternalIdIn(Set<Long> externalId);
 
--- src/main/java/mate/academy/springboot/rickandmortyapp/service/HttpClient.java
+++ src/main/java/mate/academy/springboot/rickandmortyapp/service/HttpClient.java
@@ -1,25 +1,29 @@
 package mate.academy.springboot.rickandmortyapp.service;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import lombok.RequiredArgsConstructor;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class HttpClient {
-    private final CloseableHttpClient httpClient;
+    private final CloseableHttpClient httpClient = HttpClients.createDefault();
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    public <T> T get(String url,Class<T> clazz) {
+    {
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public <T> T get(String url, Class<T> clazz) {
         HttpGet request = new HttpGet(url);
         try (CloseableHttpResponse response = httpClient.execute(request)) {
             return objectMapper.readValue(response.getEntity().getContent(), clazz);
         } catch (IOException e) {
-            throw new RuntimeException("Can't fetch info from URL: " + url, e);
+            throw new RuntimeException("Can't fetch indo from url: " + url, e);
         }
     }
 }
